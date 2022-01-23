6c2131257f7bd3da5ef58e3b5a4e493e440aafda
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2022-01-23 18:58:10 +0600

    Add files via upload

---
+++ Dreams.js
@@ -0,0 +1,20 @@
+  wow = new WOW({
+      boxClass: 'wow', // default
+      animateClass: 'animated', // default
+      offset: 0, // default
+      mobile: true, // default
+      live: true // default
+  })
+wow.init();
+  
+
+/* =========================================*/
+
+
+
+
+
+
+
+
+
